3f5a1c513d00450598cef1c73cc77a0851bfc45b
Yuvraj883 <kingyuvi88@gmail.com>
2021-03-20 20:20:18 +0530

    initial commit

---
+++ GettingStarted/PrintZ.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class PrintZ {
+
+    public static void main(String[] args) {
+        // Write your code here
+        System.out.println("*****");
+        System.out.println("   *");
+        System.out.println("  *");
+        System.out.println(" *");
+        System.out.println("*****");
+
+    }
+}
