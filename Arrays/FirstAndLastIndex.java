94745bc74ae677e2007b46847bd98f92ea86121d
Yuvraj883 <kingyuvi88@gmail.com>
2021-04-05 22:21:36 +0530

    Find the first and the last index of an element in an array.

---
+++ Arrays/FirstAndLastIndex.java
@@ -0,0 +1,50 @@
+import java.util.*;
+public class FirstAndLastIndex{
+    public static void main(String [] argc ){
+        Scanner scn  = new Scanner (System.in); 
+        int n = scn.nextInt(); 
+        int [] arr = new int[n]; 
+        for (int i= 0; i<n; i++){
+            arr[i] = scn.nextInt();
+        }
+         
+        int data = scn.nextInt();
+        int lo = 0; 
+        int high = arr.length -1; 
+        int li = -1; 
+        while (lo<= high){
+            int mid =(lo+high)/2;
+            if (arr[mid]>data){
+                high = mid -1;
+            }
+            else if (arr[mid]<data){
+                lo = mid+1;
+            }
+            else{
+                li = mid; 
+                lo = mid+1;
+            }
+        }
+
+        lo =0; 
+        high = arr.length-1; 
+        int fi =-1; 
+        while (lo<=high){
+            int mid = (lo+high)/2;
+            if (arr[mid]>data){
+                high = mid -1; 
+            }
+            else if (arr[mid]<data){
+                lo = mid+1;
+            }
+            else{
+                fi = mid; 
+                high = mid-1;
+            }
+        }
+        System.out.println(fi); 
+        System.out.println(li);
+
+
+    }
+}
