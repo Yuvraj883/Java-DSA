f3e2556efe25e142d4e4073898775a6d3631b311
Yuvraj883 <kingyuvi88@gmail.com>
2021-04-10 01:14:23 +0530

    Rotate the matrix by 90 degrees.

---
+++ 2D-Arrays/RotateBy90Degree.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class RotateBy90Degree {
+    public static void displayMatrix(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                arr[i][j] = scn.nextInt();
+            }
+        }
+
+        // transposing the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+
+            }
+        }
+        // reversing row by row
+        for (int i = 0; i < n; i++) {
+            int li = 0;
+            int ri = arr[0].length-1;
+            while (li <= ri) {
+                int temp = arr[i][li];
+                arr[i][li] = arr[i][ri];
+                arr[i][ri] = temp;
+                li++;
+                ri--;
+            }
+          
+        }
+        displayMatrix(arr);
+        scn.close();
+    }
+}
