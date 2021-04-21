33a1f939df9f4b256bfd632cec2be3d6f6df88be
Yuvraj883 <kingyuvi88@gmail.com>
2021-04-21 21:49:57 +0530

    Print the upper diagonal of the matrix

---
+++ 2D-Arrays/StateOfWakanda2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class StateOfWakanda2 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+    
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                arr[i][j] = scn.nextInt();
+            }
+        }
+        for (int g = 0; g < arr.length; g++) {
+            for (int i = 0, j = g; j < arr[0].length;i++ ,j++) {
+                System.out.println(arr[i][j]);
+            }
+        }
+        scn.close();
+    }
+}
