baf6fbae31f03dc07ad05e20830ce4b92a40ef35
Yuvraj883 <kingyuvi88@gmail.com>
2021-04-25 00:47:02 +0530

    CodeForces Round #707 Div2

--- Miscellaneous/NapoleanPancake.cpp
+++ Miscellaneous/NapoleanPancake.cpp
@@ -1,29 +1,41 @@
-#include <iostream> 
-using namespace std; 
-int main(){
-    long long t; 
-    cin>>t; 
-    while (t--){
-        long long pcs ; 
-        cin>>pcs; 
+#include <iostream>
+using namespace std;
+int main()
+{
+    long long t;
+    cin >> t;
+    while (t--)
+    {
+        long long pcs; //Number of layers in pancake
+        cin >> pcs;
         long long arr[pcs];
-        
-        for (long long i=0; i<pcs; i++){
-            cin>>arr[i];
+
+        for (long long i = 0; i < pcs; i++)
+        {
+            cin >> arr[i];
         }
-    long long temp = arr[pcs-1];
-        for (long long i=pcs-1; i>=0;i-- ){
-            if (temp !=0|| arr[i]!=0){
-                cout<<"1 ";
+        long long ans[pcs] = {0}; //initialising all the elements of the ans array to 0.
+        long long temp = arr[pcs - 1];
+
+        for (long long i = pcs - 1; i >= 0; i--)
+        {
+            if (temp != 0 || arr[i] != 0)
+            {
+                ans[i] = 1;
                 temp = max(temp, arr[i]);
                 temp--;
             }
-            else{
-                cout<<"0 ";
+            else
+            {
+
                 temp = arr[i];
             }
         }
-        cout<<endl;
+        for (long long i = 0; i < pcs; i++)
+        {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
     }
-    return 0; 
+    return 0;
 }
