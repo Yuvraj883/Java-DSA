6ac27bde6723aff8469d7ee424993581fa256312
Yuvraj883 <kingyuvi88@gmail.com>
2021-04-29 17:02:04 +0530

    Print all the palindrome substrings

---
+++ Strings/PalindromeSubStrings.java
@@ -0,0 +1,30 @@
+import java.util.*; 
+public class PalindromeSubStrings{
+  public static boolean isPalindrome(String str){
+        int i=0; 
+        int j= str.length()-1;
+        while (i<=j){
+            if (str.charAt(i)!=str.charAt(j)){
+                return false; 
+            }
+            else {
+                i++; 
+                j--;
+            }
+        }
+        return true; 
+    }
+    public static void main(String[] args){
+        Scanner scn = new Scanner (System.in); 
+        String str = scn.next(); 
+        for (int i =0; i<str.length(); i++){
+            for (int j=i+1; j<=str.length(); j++){
+                String temp = str.substring(i, j);
+                if (isPalindrome(temp)==true){
+                    System.out.println(temp);
+                }
+            }
+        }
+        scn.close();
+    }
+}
