52124727bf35f763b1d1426e20658f60b272bf11
Yuvraj883 <kingyuvi88@gmail.com>
2021-05-08 23:38:13 +0530

    Print steps to solve tower of hanoi

--- Recursion-Backtracking/TowerOfHanoi.java
+++ Recursion-Backtracking/TowerOfHanoi.java
@@ -1,20 +1,22 @@
-import java.util.*; 
-public class TowerOfHanoi{
-    public static void towerOfHanoi(int n, int a, int b, int c){
-        if (n ==0){
-            return ; 
+import java.util.*;
+
+public class TowerOfHanoi {
+    public static void towerOfHanoi(int n, int fromRod, int toRod, int auxRod) {
+        if (n == 0) {
+            return;
         }
-        towerOfHanoi(n-1, a,c,b); 
-        System.out.println(n +"["+ a+" -> "+b+ "]");
-         towerOfHanoi(n-1, c,b,a); 
+        towerOfHanoi(n - 1, fromRod, auxRod, toRod);
+        System.out.println(n + "[" + fromRod + " -> " + toRod + "]");
+        towerOfHanoi(n - 1, auxRod, toRod, fromRod);
     }
-    public static void main(String[] args){
-        Scanner scn = new Scanner (System.in); 
-        int n = scn.nextInt(); 
-        int a = scn.nextInt(); 
-        int b = scn.nextInt(); 
-        int c = scn.nextInt(); 
-        towerOfHanoi(n, a, b, c); 
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int a = scn.nextInt();
+        int b = scn.nextInt();
+        int c = scn.nextInt();
+        towerOfHanoi(n, a, b, c);
         scn.close();
 
     }
