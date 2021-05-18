adfc629913a99568473efc477f4b638db9b45124
Yuvraj883 <kingyuvi88@gmail.com>
2021-05-18 16:37:23 +0530

    Find out the repeating names in a string of names

---
+++ Miscellaneous/repeatingNames.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string str;
+    getline(cin, str);
+
+    vector<string> vec;
+   string temp ="";
+   for (int i=0; i<str.length(); i++){
+       if(str[i]==' '){
+           vec.push_back(temp);
+           temp ="";
+       }
+       else{
+           temp.push_back(str[i]);
+       }
+   }
+   vec.push_back(temp);
+    sort(vec.begin(), vec.end());
+    // for(auto i: vec){
+    //     cout<<i<<endl; 
+    // }
+    int count = 1; 
+    for (int i=1; i<=vec.size(); i++){
+        if(vec[i-1]== vec[i]){
+            count++;
+        }
+        else if (vec[i-1]!=vec[i] && count >1){
+            cout<<count<<" "<<vec[i-1];
+            count = 1;
+            cout<<endl;
+        }
+
+    }
+   
+
+}
