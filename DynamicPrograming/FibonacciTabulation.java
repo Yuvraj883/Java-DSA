a9cacb938c8aee67df0097ef7666e31135853c34
Yuvraj883 <kingyuvi88@gmail.com>
2021-06-04 23:47:36 +0530

    finding nth Fibonacci using tabulation

---
+++ DynamicPrograming/FibonacciTabulation.java
@@ -0,0 +1,22 @@
+import java.util.*; 
+public class FibonacciTabulation{
+    public static void main(String[] args){
+        Scanner scn = new Scanner (System.in); 
+        int n = scn.nextInt(); 
+      int ans =  fibonacci(n);
+      System.out.println(ans);
+    }
+    public static int fibonacci(int n){
+        int [] tab = new int [n+1];
+        tab[0] = 1; 
+        for (int i =1 ; i<n; i++){
+            if(i ==1){
+                tab[i]= tab[i-1];
+            }
+        else{
+            
+            tab[i]= tab[i-1]+tab[i-2];} 
+        }
+        return tab[n-1];
+    }
+}
