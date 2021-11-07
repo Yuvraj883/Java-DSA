80c110c6fdf2d26e7c1d31900cf3ee3e0103b2d9
Yuvraj883 <kingyuvi88@gmail.com>
2021-11-07 14:06:18 +0530

    Write a Java program to implement inheritance by applying various access controls to its data members and methods

---
+++ JavaPracticals 5th Sem/13_Inheritance.java
@@ -0,0 +1,25 @@
+class Super1{
+    public void Super(){
+        System.out.println("Super Class"); 
+    }
+    protected void disp(){
+        System.out.println("Super Class Disp"); 
+    }
+}
+class Sub1 extends Super{
+    public void Sub1 (){
+        System.out.println("Derived Class");
+    }
+    public void show(){
+        System.out.println("Derived class-show");
+
+    }
+}
+
+class Inherit{
+    public static void main(String[] args){
+        Sub1 p = new Sub1(); 
+        p.disp(); 
+        p.show();
+    }
+}
