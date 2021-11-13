5a23ef5971cf3e90b0e1668c99a650a9688e985b
Yuvraj883 <kingyuvi88@gmail.com>
2021-11-13 19:03:19 +0530

    Write a java program to implement class using packages.

---
+++ JavaPracticals/Package1/Class1.java
@@ -0,0 +1,9 @@
+package Package1; 
+public class Class1{
+    protected int val = 50; 
+    public void dispA(){
+        System.out.println("Class 1"); 
+        System.out.println("Value ="+val); 
+
+    }
+}
