f09cc091275c122cd9c5313aa7bc30aa0419d1c7
Yuvraj883 <kingyuvi88@gmail.com>
2021-11-16 14:30:16 +0530

    Write a Java applet program to run using browser

---
+++ JavaPracticals 5th Sem/28_Applet.java
@@ -0,0 +1,9 @@
+import java.util.*; 
+import java.awt.Graphics; 
+import java.applet.Applet; 
+ class App2 extends Applet{
+public void paint(Graphics g){
+    g.drawString("Hello Java", 5,20); 
+    
+}
+}
