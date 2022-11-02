d48a3af01f29eac80ea99704cc8e6bd648a0b0ce
Yuvraj <kingyuvi88@gmail.com>
2022-11-02 23:51:48 +0530

    WAP to perform Linear and Binary Search on a linear array

---
+++ Maharaja Surajmal Institute of Technology/DSA in C/Linear&BinarySearch.c
@@ -0,0 +1,64 @@
+#include <stdio.h> 
+
+void linearSearch(int arr[],int n, int key){
+    int isPresent=0; 
+    for (int i=0; i<n && isPresent==0; i++){
+        if(arr[i]==key){
+            printf("Number found in the array on index %d using Linear Search.\n",i);
+            isPresent=1; 
+        }
+
+    } 
+    if(!isPresent){
+    printf("Number doesn't exist in the array!!\n"); 
+
+    }
+}
+
+void binarySearch(int arr[], int n, int key){
+    int low = 0; 
+    int high = n-1; 
+    int mid = (low+high)/2; 
+    int isPresent = 0; 
+
+    while(low<high && isPresent ==0){
+        if(arr[mid]==key){
+            printf("Number found in the array on index %d through Binary Search.\n",mid);
+            isPresent=1;  
+        }
+        else if(arr[mid]>key){
+            high = mid-1; 
+            mid = (low+high)/2; 
+        }
+        else if(arr[mid]<key){
+            low = mid+1; 
+            mid = (low+high)/2; 
+        }
+    }
+     if(!isPresent){
+        printf("Number doesn't exist in the array!!\n");
+     }
+
+}
+
+
+int main(){
+    int n; 
+    printf("Enter the length of array: "); 
+    scanf("%d", &n); 
+     
+    int arr[n]; 
+    printf("Enter the array elements: "); 
+    for (int i=0; i<n; i++){
+        scanf("%d",&arr[i]); 
+    }
+    int key; 
+    printf("Enter the number you want to search in the array: "); 
+    scanf("%d", &key); 
+
+
+    linearSearch(arr, n, key); 
+    binarySearch(arr, n, key); 
+    return 0; 
+
+}
