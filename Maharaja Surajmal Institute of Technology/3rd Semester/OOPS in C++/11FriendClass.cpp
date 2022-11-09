f02bf0b241da187674dc422fdf73c59f90ba15f4
Yuvraj <kingyuvi88@gmail.com>
2022-11-09 17:24:23 +0530

    WAP to show friend class

---
+++ Maharaja Surajmal Institute of Technology/3rd Semester/OOPS in C++/11FriendClass.cpp
@@ -0,0 +1,25 @@
+#include <iostream> 
+using namespace std; 
+
+class Class1{
+    friend class Class2; //The class which is declared as a friend 
+                         //to a class can access all of its private, protected and public members. 
+    int data; 
+    public : 
+    Class1(){
+        data=9; 
+    }
+};
+
+class Class2{
+public: 
+void showData(Class1 obj){
+    cout<<(obj.data);
+}
+}; 
+
+int main(){
+    Class1 obj1; 
+    Class2 obj2; 
+    obj2.showData(obj1); 
+}
