f53fde473a90ede08f9299b1d5c5be3db9630eaa
Yuvraj <kingyuvi88@gmail.com>
2022-11-12 20:37:13 +0530

    WAP to take two matrix input and perform addition, multipication and transpose of the matrix.

--- Maharaja Surajmal Institute of Technology/3rd Semester/DSA in C/01(b)2DArray.c
+++ Maharaja Surajmal Institute of Technology/3rd Semester/DSA in C/01(b)2DArray.c
@@ -18,7 +18,17 @@ void addMatrix(int r1, int c1, int arr1[][c1], int r2, int c2, int arr2[][c2]){
             sum[i][j]=arr1[i][j]+arr2[i][j]; 
         }
     }
+    printf("\n Following is the sum of two matrices: "); 
     printMatrix(r1,c1,sum); 
+}
+
+void transposeMatrix(int rows, int cols, int arr[][cols]){
+    for(int i=0;i<cols; i++){
+        for(int j=0; j<rows; j++){
+            printf("%d ",arr[j][i]);
+        }
+        printf("\n");
+    }
 }
 
 int main(){
@@ -44,13 +54,36 @@ int main(){
         scanf("%d",&arr2[i][j]); 
     }
    }
-// printf("First Matrix\n");
-//    printMatrix( rows1, cols1,arr1); 
-// printf("\n");
 
-// printf("Second Matrix\n");
-//    printMatrix( rows2, cols2,arr2); 
-// printf("\n");
-    addMatrix(rows1,cols1, arr1, rows2, cols2, arr2); 
+    addMatrix(rows1,cols1, arr1, rows2, cols2, arr2);
+    if(rows1!=cols2){
+        printf("Invalid Input! These two matrices can't be multiplied together.");
+    } 
+
+    else{
+        int prod[rows1][cols2];
+           for(int i=0; i<rows1; i++){
+        for(int j=0; j<cols2; j++){
+            prod[i][j]=0;
+        }
+        
+    }
+
+        for(int i=0; i<rows1; i++){
+            for(int j=0; j<cols2; j++){
+                for(int k=0; k<cols2; k++){
+                    prod[i][j]+= arr2[k][j] * arr1[i][k]; 
+                }
+            }
+        }
+        printf("Product of the two matrices is: \n");
+    
+    printMatrix(rows1, cols2, prod);
+    }
+    printf("Following is the transpose of the first matrix: \n"); 
+    transposeMatrix(rows1,cols1,arr1);
+    printf("Following is the transpose of the second matrix: \n"); 
+    transposeMatrix(rows2,cols2,arr2); 
+ 
     return 0;
 }
