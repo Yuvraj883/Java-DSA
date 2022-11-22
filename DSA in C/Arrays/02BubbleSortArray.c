4ca54fbd9c7e4403b06082ad4b36f5cfba0e18e5
Yuvraj <kingyuvi88@gmail.com>
2022-11-22 20:52:35 +0530

    Bubble sort a linear array.

--- DSA in C/Arrays/02BubbleSortArray.c
+++ DSA in C/Arrays/02BubbleSortArray.c
@@ -1,13 +1,38 @@
 #include <stdio.h> 
 
+void printArray(int arr[], int n){
+    for(int i =0; i<n; i++){
+        printf("%d ", arr[i]); 
+    }
+}
+
+void bubbleSort(int arr[], int n){
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+            if(arr[i]>arr[j]){
+                int temp = arr[i]; 
+                arr[i] = arr[j]; 
+                arr[j] = temp; 
+            }
+        }
+    }
+}
+
 int main(){
     int n; 
-    printf("Enter the array length: "); 
+    printf("Enter the length of the array: "); 
     scanf("%d",&n); 
 
     int arr[n]; 
     printf("Enter the array elements: "); 
     for(int i=0; i<n; i++){
-        
+        scanf("%d",&arr[i]); 
     }
+
+    printf("Array: "); 
+    printArray(arr, n); 
+    printf("\nSorted Array: "); 
+    bubbleSort(arr,n); 
+    printArray(arr, n);
+
 }
