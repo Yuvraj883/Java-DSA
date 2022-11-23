7b1fc5c7f6b10317e095851c8a8b7092eb2d896b
Yuvraj <kingyuvi88@gmail.com>
2022-11-24 00:56:09 +0530

    https://leetcode.com/problems/delete-node-in-a-linked-list/

---
+++ LeetCode/LinkedList/DeleteNode.c
@@ -0,0 +1,12 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+void deleteNode(struct ListNode* node) {
+    
+    node->val = node->next->val; 
+    node->next = node->next->next;
+}
