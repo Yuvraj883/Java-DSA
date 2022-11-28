06f4d040361830ffc8a5e8aad348c1f7b757a034
Yuvraj <kingyuvi88@gmail.com>
2022-11-28 20:08:29 +0530

    https://leetcode.com/problems/merge-two-sorted-lists/

--- LeetCode/LinkedList/MergeSortedList.c
+++ LeetCode/LinkedList/MergeSortedList.c
@@ -6,21 +6,48 @@
  * };
  */
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
-    struct ListNode* dummy = (struct ListNode*)malloc(sizeof(struct ListNode)); 
-    
-   if(list1->val=<list2->val){
-       dummy->next = list1; 
-       list1= list1->next; 
-   }
-   else{
-       dummy->next = list2; 
-       list2 = list->next; 
-   }
-   
-    while(list1->next!=NULL && list2->next!=NULL){
-        if(list1->val<list2->val){
-        
-        }
-    }
+struct ListNode* dummy = NULL; 
+ dummy = (struct ListNode*)malloc(sizeof(struct ListNode)); 
+ struct ListNode* l = dummy; 
+struct ListNode* l1 = list1; 
+struct ListNode* l2 = list2; 
+if(list1 == NULL){
+    return list2; 
+}
+else if(list2 == NULL){
+    return list1; 
+}
+// if(l1->val <=l2->val){
+//     dummy->next = l1; 
+//     l1 = l1->next; 
+// }
+// else{
+//     dummy->next = l2; 
+//     l2 = l2->next; 
+// }
 
+while(l1!=NULL && l2!=NULL){
+    if(l1->val<=l2->val){
+        l->next = l1;
+         l1 = l1->next;
+    }
+    else{
+        l->next = l2; 
+        l2 = l2->next; 
+    }
+    l = l->next; 
+}
+
+while(l1!=NULL){
+    l->next = l1; 
+    l1 = l1->next; 
+    l = l->next;
+}
+
+while(l2!=NULL){
+    l->next = l2; 
+    l2 = l2->next; 
+    l = l->next;
+}
+ return dummy->next; 
 }
