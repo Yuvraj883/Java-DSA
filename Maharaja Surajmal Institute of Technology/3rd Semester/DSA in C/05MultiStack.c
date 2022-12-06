2e32d7630ee7e08c71e713b7a91b4830ff9dcc33
Yuvraj <kingyuvi88@gmail.com>
2022-12-07 00:52:15 +0530

    WAP to implement multi stack.

--- Maharaja Surajmal Institute of Technology/3rd Semester/DSA in C/05MultiStack.c
+++ Maharaja Surajmal Institute of Technology/3rd Semester/DSA in C/05MultiStack.c
@@ -12,39 +12,73 @@ void push1(int *stack, int size, int *top){
     stack[*top] = val; 
 
 }
-// void push2(int *stack, int size, int *top){
-//     if(*top == size1-1){
-//         printf("Stack1 is full!!"); 
-//         return; 
-//     }
-//    int val; 
-//    printf("Enter the value you want to insert to the stack: "); 
-//    scanf("%d",&val); 
-//     *top = *top+1; 
-//     stack[*top] = val; 
-
-// }
-
-void pop1(int *stack, int size, int *top){
-    if(*top == 100-size-1){
-        printf("Stack1 is empty!!");
+void push2(int *stack, int size, int *top){
+    if(*top>99){
+        printf("Stack2 is full!!"); 
         return; 
     }
-    *top --; 
+   int val; 
+   printf("Enter the value you want to insert to the stack: "); 
+   scanf("%d",&val); 
+    *top = *top+1; 
+    stack[*top] = val; 
+
+}
+
+void pop1(int *stack, int *top){
+if(*top==-1){
+    printf("Stack1 is empty!!\n");
+    return; 
+}
+*top=*top-1; 
+if(*top==-1){
+    printf("Stack1 has been emptied!!\n");
+    return; 
+}
+}
+
+void pop2(int *stack, int size, int *top){
+    if(*top == size-1){
+        printf("Stack2 is empty!!\n");
+        return; 
+    }
+    *top=*top-1; 
+     if(*top == size-1){
+        printf("Stack2 has been emptied!!\n");
+        return; 
+    }
+}
+
+void peek1(int stack[], int top){
+    printf("%d is at the top of Stack1.\n",stack[top]); 
+}
+
+void peek2(int stack[], int top){
+    printf("%d is at the top of Stack2.\n",stack[top]); 
+
 }
 
 void Display1(int stack[], int top){
+    if(top==-1){
+        return;
+    }
     printf("Stack1: "); 
-    for(int i=0; i<top; i++){
+    for(int i=top; i>=0; i--){
         printf("%d ",stack[i]);
     }
+    printf("\n"); 
+
 }
 
-void Display2(int stack[], int size1, int top){
+void Display2(int stack[], int size, int top){
+    if(top<=100-size-1){
+        return;
+    }
     printf("Stack2: "); 
-    for(int i=size1; i<top; i++){
+    for(int i=top; i>size-1; i--){
         printf("%d ",stack[i]);
     }
+    printf("\n"); 
 }
 
 
@@ -53,11 +87,18 @@ int main(){
     printf("100 values can be stored in this multi-stack.\n"); 
     int size1=50, size2 = 50;
     int top1 = -1; 
-    int top2 = size1; 
+    int top2 = size1-1; 
     push1(stack,size1, &top1); 
-    // push1(stack, size2, &top2); 
+    push2(stack, size2, &top2); 
+    push2(stack, size2, &top2); 
+    peek1(stack, top1); 
+    peek2(stack,top2);
+    pop1(stack, &top1); 
+
+    pop2(stack,size2, &top2); 
+        
     Display1(stack, top1); 
-    Display2(stack, size1,top2); 
+    Display2(stack, size2,top2); 
 
     
 }
