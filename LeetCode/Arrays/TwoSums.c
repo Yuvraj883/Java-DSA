2f8609b89ade441d5014e951d61dfbfa2b477926
Yuvraj <kingyuvi88@gmail.com>
2022-12-10 01:30:43 +0530

    https://leetcode.com/problems/two-sum/

--- LeetCode/Arrays/TwoSums.c
+++ LeetCode/Arrays/TwoSums.c
@@ -3,16 +3,21 @@
  */
 int* twoSum(int* nums, int numsSize, int target, int* returnSize){
     int n = numsSize;
-    int *arr = (int*)malloc(2*sizeof(int));  
+      int *arr = malloc(2*sizeof(int));  
     for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            if(nums[j]==target-nums[j]){
+            arr[0] = -1; 
+                arr[1] = -1;
+        for(int j=i+1; j<n; j++){
+            if(nums[j]+nums[i]==target){
+                *returnSize = 1; 
                 arr[0] = i; 
                 arr[1] = j;
+                // printf("%d %d",i, j);
                 return arr; 
             }
         }
 
     }
+    *returnSize = 0; 
     return NULL; 
 }
