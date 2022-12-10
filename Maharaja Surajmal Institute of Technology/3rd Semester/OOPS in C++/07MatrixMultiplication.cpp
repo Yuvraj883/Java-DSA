0d635bd4494b41199fc69ea6af9b3a8e1048fc40
Yuvraj <kingyuvi88@gmail.com>
2022-12-10 09:01:59 +0530

    WAP to perform matrix multiplication.

--- Maharaja Surajmal Institute of Technology/3rd Semester/OOPS in C++/07MatrixMultiplication.cpp
+++ Maharaja Surajmal Institute of Technology/3rd Semester/OOPS in C++/07MatrixMultiplication.cpp
@@ -1,88 +1,55 @@
 #include <iostream> 
 using namespace std; 
 
-// void matrixInput(int rows, int cols, int arr[rows][cols]){
-   
-    
-// }
-// void multiplyMatrices(int r1, int c1, int arr[][10], int r2, int c2, int arr2[][10]){
-//     if(c1!=r2){
-//         cout<<"Multiplication can't be performed on these two matrices.";
-//      return;    
-//     }
-//     int product[r1][c2]; 
-//     for (int i=0; i<r1; i++){
-//         for (int j=0; j<c2; j++){
-//             for (int k=0; k<r1; k++){
-//                 product[i][j]+=arr[k][j]*arr2[i][k];
-//             }
-//         }
-//     }
-
-//     for(int i=0; i<r1; i++){
-//         for (int j=0; j<c2; j++){
-//             cout<<product[i][j];
-//         }
-//         cout<<endl;
-//     }
-    
-// }
 int main(){
-    int rows1, cols1, rows2, cols2; 
-    cout<<"Enter the number of rows and cols of the first matrix: "; 
-    cin>>rows1>>cols1; 
-
-    cout<<"Enter the elements of the first matrix:\n "; 
-   
-    int arr[10][10], arr2[10][10];
-      for(int i=0; i<rows1; i++){
-        for(int j=0; j<cols1; j++){
-            cin>>arr[i][j]; 
+    int r1,c1; 
+    cout<<"Enter the number of rows and cols in the matrix1: "; 
+    cin>>r1>>c1; 
+    int a[r1][c1]; 
+    cout<<"Enter the elements of the matrix1: "; 
+    for(int i=0; i<r1; i++){
+        for(int j=0; j<c1; j++){
+            cin>>a[i][j]; 
         }
     }
 
-    //  matrixInput(rows1, cols1, arr);     
-    //  for(int i=0; i<rows1; i++){
-    //     for(int j=0; j<cols1; j++){
-    //         cout<<arr[i][j]; 
-    //     }
-    //     cout<<endl; 
-    // }
+    int r2,c2; 
+    cout<<"Enter the number of rows and cols in the matrix2:  "; 
+    cin>>r2>>c2;
+    int b[r2][c2]; 
+    cout<<"Enter the elements of the matrix2: "; 
+    for(int i=0; i<r2; i++){
+        for(int j=0; j<c2; j++){
+            cin>>b[i][j]; 
+        }
+    }
+
+    if(r1!=c2){
+        cout<<"Invalid Input!!"<<endl; 
+        return 0; 
+    }
+
+    int c[r1][c2]; 
+    for(int i=0; i<r1; i++){
+        for(int j=0; j<c2; j++){
+            c[i][j]=0;
+        }
+    }
     
-cout<<"Enter the number of rows and cols in second matrix: "; 
-cin>>rows2>>cols2; 
-
-cout<<"Enter the elements of second matrix: \n"; 
-for(int i=0; i<rows2; i++){
-    for (int j=0; j<cols2; j++){
-        cin>>arr2[rows2][cols2]; 
-    }
-}
-// multiplyMatrices(rows1,cols1,arr,rows2,cols2,arr2); 
-if(rows1!=cols2){
-    cout<<"Invalid Input"<<endl; 
-    // return 0; 
-}
-int product[10][10]; 
-for(int i=0; i<rows1; i++){
-    for(int j=0; j<cols2; j++){
-        product[i][j]=0;
-    }
-    // cout<<endl;
-}
-for(int i=0; i<rows1; i++){
-    for(int j=0; j<cols2; j++){
-        for(int k=0; k<cols2; k++){
-            product[i][j] += (arr[i][k])*(arr2[k][j]);
+for(int i=0; i<r1; i++){
+    for(int j=0; j<c2; j++){
+        for(int k=0; k<r1; k++){
+            c[i][j]=a[i][k]*b[k][j]; 
         }
     }
 }
 
-for(int i=0; i<rows1; i++){
-    for(int j=0; j<cols2; j++){
-        cout<<product[i][j]<<" ";
+for(int i=0; i<r1;i++ ){
+    for(int j=0; j<c2; j++){
+        cout<<c[i][j]<<" "; 
     }
     cout<<endl;
 }
+
 
 }
