c73f7307c197ad11d833df0b48722d4a9075a8bb
Yuvraj <kingyuvi88@gmail.com>
2022-12-10 09:22:46 +0530

    WAP to show friend class to a specified function.

---
+++ Maharaja Surajmal Institute of Technology/3rd Semester/OOPS in C++/12FriendClassToASpecificFunction.cpp
@@ -0,0 +1,28 @@
+#include<iostream> 
+using namespace std; 
+
+class B; 
+class A{
+    private: 
+    int x; 
+    public: 
+    void setValue(){
+        cout<<"Enter the value of X: ";  
+        cin>>x;
+    }
+    friend class B; 
+}; 
+
+class B{
+    public: 
+    void getValue(A &obj){
+        cout<<"Value of X is = "<<obj.x<<endl; 
+    }
+};
+
+int main(){
+    A a; 
+    a.setValue();
+    B b; 
+    b.getValue(a);
+}
