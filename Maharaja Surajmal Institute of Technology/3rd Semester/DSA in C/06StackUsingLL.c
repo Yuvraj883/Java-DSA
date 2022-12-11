837210b0a04f20b8a3969c6fa2024f1376e3cf43
Yuvraj <kingyuvi88@gmail.com>
2022-12-11 18:13:16 +0530

    WAP to implement Stack using Linked List.

--- Maharaja Surajmal Institute of Technology/3rd Semester/DSA in C/06StackUsingLL.c
+++ Maharaja Surajmal Institute of Technology/3rd Semester/DSA in C/06StackUsingLL.c
@@ -1,93 +1,87 @@
-#include <stdio.h> 
-#include <stdlib.h> 
+#include<stdio.h> 
+#include<stdlib.h> 
 
-struct Stack{
+struct Node{
     int data; 
-    struct Stack* next; 
+    struct Node* next; 
 }; 
 
-void push (struct Stack** top){
-    struct Stack* temp = (struct Stack*)malloc(sizeof(struct Stack)); 
-    if(temp==NULL){
-        //There's is no space left in the memory itself.
-        printf("Stack Overflow!!"); 
+void push(struct Node** top){
+    struct Node* el = (struct Node*)malloc(sizeof(struct Node)); 
+    printf("Enter the value you want to insert: "); 
+    scanf("%d",&el->data); 
+    if(top==NULL){
+        el->next = NULL; 
+        *top = el; 
+    }
+    else{
+        el->next=*top; 
+        *top= el; 
+    }
+    
+}
+
+void pop(struct Node** top){
+    struct Node* temp = NULL; 
+    temp = *top; 
+    *top = temp->next; 
+    free(temp); 
+
+}
+
+void peek(struct Node* top){
+    if(top==NULL){
+        printf("Stack is empty.\n"); 
         return;
     }
-    else{
-    printf("\nEnter the value you want to insert to the stack: "); 
-    scanf("%d",&temp->data); 
-    }
-    if(*top ==NULL){
-        temp->next = NULL; 
-        *top = temp; 
-    }
-    else{
-          temp->next = *top; 
-         *top = temp; 
-    }
+    printf("%d is on the top of the stack.\n",top->data); 
 }
 
-void pop(struct Stack **top){
-  struct Stack* temp;
 
- if(*top == NULL){
-    printf("Stack underflow!!"); 
- }
+void printStack(struct Node* n){
+    if(n==NULL){
+        printf("Stack underflow!!\n");
+        return;
+    }
+    while(n!=NULL){
+        if(n->data != 0){
+        printf("%d ", n->data);
+        }
+        n = n->next; 
+    }
+        printf("\n");
 
-  temp = *top; 
-  *top = temp->next; 
-  free(temp); 
 
 }
 
-void peek(struct Stack* top){
-    printf("\nThe value at the top is %d", top->data); 
-}
-
-void printStack(struct Stack* n){
-    printf("\nStack: "); 
-    if(n->next==NULL){
-        printf("Stack is empty!"); 
-    }
-    else{
-    while(n->next!=NULL){
-      
-         printf("%d ",n->data); 
-        n = n->next;
-    }
-    printf("\n"); 
-    }
-}
 
 int main(){
-    struct Stack* top=NULL; 
-    top = (struct Stack*)malloc(sizeof(struct Stack)); 
-    // printStack(head); 
+    struct Node* top= NULL; 
+    int choice; 
+    while(1){
+        printf("Operations supported by the stack:-\n 1.Push\n 2.Pop\n 3.Peek\n 4.Print \n 5.Exit\n"); 
+        printf("Enter your choice: "); 
+        scanf("%d", &choice); 
+        switch(choice){
+            case 1:
+            push(&top); 
+            break; 
+            case 2: 
+            pop(&top);
+            break; 
+            case 3: 
+            peek(top);
+            break;
+            case 4: 
+            printStack(top); 
+            break;
+            case 5:  
+            exit(0); 
+            break; 
+            default: 
+            printf("Invalid Input!\n"); 
+            break;
+        }
+    }
     
-    // int choice; 
-    // while(1){
-    //     printf("Follwing are the operations that can be performed on the Stack. \n 0.Exit \n 1.Push \n 2.Pop \n 3.Peek \n 4.Print\n");
-    //     printf("Enter your choice: "); 
-    //     scanf("%d",&choice); 
-    //     switch(choice){
-    //         case 0: exit(0); 
-    //         break; 
-    //         case 1: push(&top); 
-    //         break; 
-    //         case 2: pop(&top); 
-    //         break;
-    //         case 3: peek(top); 
-    //         break; 
-    //         case 4: printStack(top); 
-    //         break;
-    //     }
-    // }
-    // pop(&top); 
-    push(&top); 
-    push(&top); 
-    push(&top); 
-    pop(&top); 
-   
-    printStack(top); 
-
 }
