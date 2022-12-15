22ec0c266c4ad9c5f9a5fbafbef3078f9c1d8036
Yuvraj <kingyuvi88@gmail.com>
2022-12-15 17:08:10 +0530

    WAP to demonstrate copy constructor.

---
+++ Maharaja Surajmal Institute of Technology/3rd Semester/OOPS in C++/14CopyConstructor.cpp
@@ -0,0 +1,36 @@
+#include<iostream> 
+using namespace std; 
+
+class Student {
+    private : 
+    string name; 
+    int roll_no; 
+    double res;  
+    public: 
+    Student(string name, int roll_no, double res){
+        this->name = name;
+        this->roll_no = roll_no; 
+        this->res= res; 
+    }
+
+    Student(Student &s){
+        name = s.name; 
+        roll_no = s.roll_no; 
+        res = s.res; 
+    }
+
+    void display(){
+        cout<<roll_no<<" "<<name<<" "<<res;
+        cout<<endl;
+    }
+
+};
+
+int main(){
+Student s1("Yuvraj", 61, 99.9); 
+s1.display();
+
+Student s2(s1);
+s2.display();
+
+}
