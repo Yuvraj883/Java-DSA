eb7c5bfee096940fe2dd78bca183c119c5dd8138
Yuvraj <kingyuvi88@gmail.com>
2023-02-04 07:05:24 +0530

    Sort an array using Bubble Sort.

--- DSA in C/Arrays/02BubbleSortArray.c
+++ DSA in C/Arrays/02BubbleSortArray.c
@@ -8,11 +8,11 @@ void printArray(int arr[], int n){
 
 void bubbleSort(int arr[], int n){
     for(int i=0; i<n; i++){
-        for(int j=i+1; j<n; j++){
-            if(arr[i]>arr[j]){
-                int temp = arr[i]; 
-                arr[i] = arr[j]; 
-                arr[j] = temp; 
+        for(int j=0; j<n-i-1; j++){
+            if(arr[j]>arr[j+1]){
+                int temp = arr[j]; 
+                arr[j] = arr[j+1]; 
+                arr[j+1] = temp; 
             }
         }
     }
