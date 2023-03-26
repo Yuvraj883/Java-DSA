8ea4be3a03f5ca0be368ab381080b5f4a6f59035
Yuvraj <kingyuvi88@gmail.com>
2023-03-26 23:47:01 +0530

    WAP to demonstrate Scanner Class in java.

---
+++ Maharaja Surajmal Institute of Technology/4th Semester/Java/01(b)EvaluateExpression.java
@@ -0,0 +1,14 @@
+import java.util.*; 
+class Demo{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20; 
+        int c = 30; 
+        int z = 40; 
+        int x = 60; 
+        int y = 70; 
+
+        System.out.println("i. (a/c)z/a = "+(a/c)*z/a); 
+        System.out.println("ii. c++ +b/x-y = "+(c++ +b/x-y)); 
+    }
+}
---
+++ Maharaja Surajmal Institute of Technology/4th Semester/Java/01(c)ScannerClass.java
@@ -0,0 +1,9 @@
+import java.util.*; 
+class Demo{
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in); 
+        System.out.println("Enter your name: "); 
+        String name = scn.nextLine(); 
+        System.out.println("Hello, "+name+"."); 
+    }
+}
