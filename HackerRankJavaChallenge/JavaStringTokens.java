45ee483a8beaadc8f6de3a38bea2ea63bdf7e39d
Yuvraj <kingyuvi88@gmail.com>
2023-07-13 00:31:56 +0530

    https://www.hackerrank.com/challenges/java-string-tokens/problem?isFullScreen=true 4/11 test cases failed

--- HackerRankJavaChallenge/JavaStringTokens.java
+++ HackerRankJavaChallenge/JavaStringTokens.java
@@ -7,35 +7,10 @@ public class Solution {
         Scanner scan = new Scanner(System.in);
         String s = scan.nextLine();
         // Write your code here.
-        // String[] arrayOfString = s.replaceAll(","," ").split("[' '!._,?'@'[,] ]");
-        //  String[] arrayOfString = s.split("\\w+");
-        ArrayList<String> arr = new ArrayList<String>();
-          String temp="";
-        for(int i=0; i<s.length(); i++){
-            char ch = s.charAt(i);
-          
-            if(ch==' '|| ch=='!'|| ch==',' || ch=='@' || ch=='?' || ch==39){
-             if( temp!=""){
-                   arr.add(temp);
-                temp="";
-             }
-               
-              
-                
-               
-            }
-            else{
-                 
-                temp += ch;
-            }
-        }
-        // System.out.println(arrayOfString.length); 
-        // for(int i =0; i<arrayOfString.length; i++) {
-        //     System.out.println(arrayOfString[i]);
-        // }
-        System.out.println(arr.size());
-        for(int i=0; i<arr.size();i++ ){
-            System.out.println(arr.get(i));
+        String [] strArr = s.split("[!,?._'@ ' ']+");
+        System.out.println(strArr.length);
+        for(String temp : strArr){
+            System.out.println(temp);
         }
         scan.close();
     }
