ff39399708c1362350126a6ae6b4279c030536e5
Yuvraj <kingyuvi88@gmail.com>
2023-08-20 01:48:48 +0530

    https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/submissions/?envType=study-plan-v2&envId=leetcode-75

---
+++ LeetCode/Arrays/1431.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> result = new ArrayList<>();
+        int max = candies[0]; 
+        for(int i=1; i<candies.length; i++){
+            if(candies[i]>max){
+                max = candies[i];
+            }
+        }
+
+        for(int i=0; i<candies.length; i++){
+            if(candies[i]+extraCandies>=max){
+                result.add(true);
+            }
+            else{
+                result.add(false);
+            }
+        }
+        
+        return result; 
+    }
+}
